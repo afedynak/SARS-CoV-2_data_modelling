03bed19f3967795415121a8d56d27d5edfe73a6b
admin <admin@admins-Laptop.local>
2025-07-29 09:56:04 -0400

    Updated files

---
+++ scripts/library.R
@@ -0,0 +1,32 @@
+# Common packages used my projects:
+  pacman::p_load(
+     knitr,
+     readxl, 
+     lubridate,
+     ggplot2,
+     dplyr,
+     
+     tidyr,
+     tidyverse,
+     scales,
+     formatR,
+     ggthemes,
+    
+     RColorBrewer,
+     ggpubr,
+     zoo,
+     cowplot,
+     patchwork,
+    
+     pracma,
+     purrr,
+     ggpmisc,
+     rmdformats,
+     ggprism,
+     ggcorrplot,
+     
+     colorspace
+    )
+
+# Set the default ggplot theme:
+  theme_set(theme_bw())
